da7075a331c7a31ab331af444e660b1f0425f1a6
German-Jala <German.Alvarez@jala.university>
2026-05-15 01:05:33 -0400

    rama local

---
+++ Modules/Users/Application/Ports/In/ILoginUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiProyecto.Modules.Users.Application.Ports.In
+{
+    internal interface ILoginUserUseCase
+    {
+    }
+}
