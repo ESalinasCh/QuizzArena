cc0f5df0a5e5bf53a6b9287ecafe90b2e2f9b734
Luigi-Jala <luigi.cabrera@jala.university>
2026-05-31 11:47:23 -0500

    feat: Azurite Set up

---
+++ QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Application/Ports/Out/IBlobRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizzArena.DocumentProcessing.Application.Ports.Out;
+
+public interface IBlobRepository
+{
+    Task<string> UploadFileAsync();
+}
---
+++ QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Services/BlobRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuizzArena.DocumentProcessing.Application.Ports.Out;
+
+namespace QuizzArena.DocumentProcessing.Infrastructure.Adapters.Out.Services;
+
+public class BlobRepository : IBlobRepository
+{
+    public Task<string> UploadFileAsync() => throw new NotImplementedException();
+
+}
