1575882b6094e3e63b60d372c8705b8d957c5221
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-11 09:59:55 -0500

    refactor: change tab for spaces C# standards

--- QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/SubmitAnswers/SubmitAnswersRequestDto.cs
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/SubmitAnswers/SubmitAnswersRequestDto.cs
@@ -1,14 +1,14 @@
-
+﻿
 namespace QuizzArena.Quizzing.Application.DTOs.SubmitAnswers;
 
 public class SubmitAnswersRequestDto
 {
-	public required List<SubmitAnswerBody> Answers { get; set; }
+    public required List<SubmitAnswerBody> Answers { get; set; }
 }
 
 public record SubmitAnswerBody
 (
-	Guid QuestionId,
-	Guid SelectedOptionId,
-	DateTimeOffset AnsweredAt
+    Guid QuestionId,
+    Guid SelectedOptionId,
+    DateTimeOffset AnsweredAt
 );
--- QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/SubmitAnswers/SubmitAnswersResponseDto.cs
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/SubmitAnswers/SubmitAnswersResponseDto.cs
@@ -1,22 +1,22 @@
-
+﻿
 namespace QuizzArena.Quizzing.Application.DTOs.SubmitAnswers;
 
 public class SubmitAnswersResponseDto
 {
-	public Guid AttemptId { get; set; }
-	public int ScorePercentage { get; set; }
-	public int CorrectCount { get; set; }
-	public int IncorrectCount { get; set; }
-	public int TotalQuestions { get; set; }
-	public required List<QuestionResultDto> Questions { get; set; }
+    public Guid AttemptId { get; set; }
+    public int ScorePercentage { get; set; }
+    public int CorrectCount { get; set; }
+    public int IncorrectCount { get; set; }
+    public int TotalQuestions { get; set; }
+    public required List<QuestionResultDto> Questions { get; set; }
 }
 
 public record QuestionResultDto
 (
-	Guid Id,
-	int Number,
-	string Text,
-	Guid SelectedOptionId,
-	Guid CorrectOptionId,
-	bool IsCorrect
+    Guid Id,
+    int Number,
+    string Text,
+    Guid SelectedOptionId,
+    Guid CorrectOptionId,
+    bool IsCorrect
 );
