ab200caaa9b8356ec27987d8f379dca59e66de4a
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-11 09:58:02 -0500

    feat: create dtos for SubmitAnwers

---
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/SubmitAnswers/SubmitAnswersRequestDto.cs
@@ -0,0 +1,14 @@
+
+namespace QuizzArena.Quizzing.Application.DTOs.SubmitAnswers;
+
+public class SubmitAnswersRequestDto
+{
+	public required List<SubmitAnswerBody> Answers { get; set; }
+}
+
+public record SubmitAnswerBody
+(
+	Guid QuestionId,
+	Guid SelectedOptionId,
+	DateTimeOffset AnsweredAt
+);
