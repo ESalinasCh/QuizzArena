dd1ee4fb35a8414cfa26f905432dcb04dd26c02c
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-11 14:18:11 -0500

    feat: add MatchAttemptsController and ISubmitAnswerUseCase

---
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/In/Web/MatchAttemptsController.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using QuizzArena.Quizzing.Application.DTOs.SubmitAnswers;
+using QuizzArena.Quizzing.Application.Ports.In;
+
+namespace QuizzArena.Quizzing.Infrastructure.Adapters.In.Web;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/match-attempts/{attempt-id}/submit")]
+public class MatchAttemptsController(
+    ISubmitAnswersUseCase submitAnswersUseCase
+) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<SubmitAnswersResponseDto>> SubmitAnswers(SubmitAnswersRequestDto dto)
+    {
+        SubmitAnswersResponseDto response = await submitAnswersUseCase.Execute(dto);
+        return Ok(response);
+    }
+}
