b7c22cd0a9ff355e6d37ad8826f7b136a7308cd3
Edson <Edson.Quispe@jala.university>
2026-06-15 10:35:32 -0400

    feat: add justification on questions

--- QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/Out/Persistence/Migrations/20260615143400_AddJustificationOnQuestions.cs
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/Out/Persistence/Migrations/20260615143400_AddJustificationOnQuestions.cs
@@ -2,31 +2,30 @@
 
 #nullable disable
 
-namespace QuizzArena.Quizzing.Infrastructure.Adapters.Out.Persistence.Migrations
+namespace QuizzArena.Quizzing.Infrastructure.Adapters.Out.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddJustificationOnQuestions : Migration
 {
     /// <inheritdoc />
-    public partial class AddJustificationOnQuestions : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "Justification",
-                schema: "quizzing",
-                table: "question",
-                type: "character varying(600)",
-                maxLength: 600,
-                nullable: false,
-                defaultValue: "");
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "Justification",
+            schema: "quizzing",
+            table: "question",
+            type: "character varying(600)",
+            maxLength: 600,
+            nullable: false,
+            defaultValue: "");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Justification",
-                schema: "quizzing",
-                table: "question");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Justification",
+            schema: "quizzing",
+            table: "question");
     }
 }
