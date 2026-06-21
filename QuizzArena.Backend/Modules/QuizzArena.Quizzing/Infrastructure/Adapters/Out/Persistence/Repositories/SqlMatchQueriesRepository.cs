f21fe145ed6ec808b7f6501c808a6b18814b95b6
RoBra18 <brand8n123@gmail.com>
2026-06-21 17:19:00 -0400

    feat: allowing match attempt filtering by match mode

--- QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/Out/Persistence/Repositories/SqlMatchQueriesRepository.cs
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/Out/Persistence/Repositories/SqlMatchQueriesRepository.cs
@@ -9,45 +9,58 @@ internal class SqlMatchQueriesRepository(QuizzingDbContext context) : IMatchQuer
 {
     public async Task<List<MatchAttempt>> GetAttemptsByStudentId(Guid studentId, MatchAttemptFilters filters)
     {
-        IQueryable<MatchAttempt> query = context.MatchAttempts
+        var query = context.MatchAttempts
          .AsNoTracking()
-         .Where(x => x.UserId == studentId);
+         .Where(x => x.UserId == studentId)
+         .Join(
+            context.Matches.AsNoTracking(),
+            matchAttempt => matchAttempt.MatchId,
+            match => match.Id,
+            (matchAttempt, match) => new { MatchAttempt = matchAttempt, Match = match }
+            ).AsQueryable();
+
+        if (filters.MatchMode.HasValue)
+        {
+            query = query.Where(x=> x.Match.Mode == filters.MatchMode);
+        }
 
         if (filters.MatchId.HasValue)
         {
-            query = query.Where(x => x.MatchId == filters.MatchId);
+            query = query.Where(x => x.MatchAttempt.MatchId == filters.MatchId);
         }
 
         if (filters.Status.HasValue)
         {
-            query = query.Where(x => x.Status == filters.Status);
+            query = query.Where(x => x.MatchAttempt.Status == filters.Status);
         }
 
         if (filters.MinScore.HasValue)
         {
-            query = query.Where(x => x.Score >= filters.MinScore);
+            query = query.Where(x => x.MatchAttempt.Score >= filters.MinScore);
         }
 
         if (filters.MaxScore.HasValue)
         {
-            query = query.Where(x => x.Score <= filters.MaxScore);
+            query = query.Where(x => x.MatchAttempt.Score <= filters.MaxScore);
         }
 
         if (filters.StartedFrom.HasValue)
         {
-            query = query.Where(x => x.StartDateTime >= filters.StartedFrom);
+            query = query.Where(x => x.MatchAttempt.StartDateTime >= filters.StartedFrom);
         }
 
         if (filters.StartedTo.HasValue)
         {
-            query = query.Where(x => x.StartDateTime <= filters.StartedTo);
+            query = query.Where(x => x.MatchAttempt.StartDateTime <= filters.StartedTo);
         }
 
         return await query
-            .OrderByDescending(x => x.StartDateTime)
+            .OrderByDescending(x => x.MatchAttempt.StartDateTime)
             .Skip((filters.Page - 1) * filters.PageSize)
             .Take(filters.PageSize)
+            .Select(x => x.MatchAttempt)
             .ToListAsync();
+        
     }
 
     public async Task<List<Match>> GetMatchesByIds(List<Guid> matchIds)
