abf376303995e9e5d7264f26ee91e899a32347ed
davidruizeJala <david.ruiz@jala.university>
2026-06-25 10:23:35 -0500

    fix(dto): correct formatting of property declaration in CreateQuizResponseDto

--- QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/Quiz/CreateQuizResponseDto.cs
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/Quiz/CreateQuizResponseDto.cs
@@ -5,7 +5,7 @@ namespace QuizzArena.Quizzing.Application.DTOs.Quiz;
 
 public class CreateQuizResponseDto
 {
-    public Guid Id {  get; set; }
+    public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public QuizStatus Status { get; set; } = QuizStatus.draft;
