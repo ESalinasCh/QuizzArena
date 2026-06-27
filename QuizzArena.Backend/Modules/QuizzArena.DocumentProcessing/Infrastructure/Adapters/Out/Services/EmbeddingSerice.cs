bf31a2c4aa6b6180b66f48c091a21ad6c1ddeda7
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-27 14:44:40 -0500

    feat: create sentence splitter static class as helper, create files for embedding services

---
+++ QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Services/EmbeddingSerice.cs
@@ -0,0 +1,8 @@
+﻿using QuizzArena.DocumentProcessing.Application.Ports.Out;
+
+namespace QuizzArena.DocumentProcessing.Infrastructure.Adapters.Out.Services;
+
+public class EmbeddingService : IEmbeddingService
+{
+
+}
