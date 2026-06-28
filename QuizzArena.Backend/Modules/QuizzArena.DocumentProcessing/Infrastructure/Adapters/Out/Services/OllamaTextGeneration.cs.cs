75658e979fdd66889a26d81266a1569485f0da68
Luigi-Jala <luigi.cabrera@jala.university>
2026-06-28 15:24:00 -0500

    refactor(OllamaTextGeneration): remove unnecessary using directive for Http.Json

--- QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Services/OllamaTextGeneration.cs.cs
+++ QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Services/OllamaTextGeneration.cs.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Schema;
 using QuizzArena.DocumentProcessing.Application.Ports.Out;
