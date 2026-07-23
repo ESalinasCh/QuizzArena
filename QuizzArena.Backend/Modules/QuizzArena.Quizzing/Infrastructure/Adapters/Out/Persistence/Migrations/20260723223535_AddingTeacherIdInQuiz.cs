97680cc13b7ad234cda7a66b202705947a5b559f
RoBra18 <brand8n123@gmail.com>
2026-07-23 18:46:43 -0400

    chore: add teacherID for quiz entity

---
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Infrastructure/Adapters/Out/Persistence/Migrations/20260723223535_AddingTeacherIdInQuiz.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizzArena.Quizzing.Infrastructure.Adapters.Out.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddingTeacherIdInQuiz : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<Guid>(
+            name: "TeacherId",
+            schema: "quizzing",
+            table: "quiz",
+            type: "uuid",
+            nullable: true);
+
+        migrationBuilder.Sql("""
+    UPDATE quizzing.quiz
+    SET "TeacherId" = '959d0300-4473-4198-b551-6c1c6fb214dc';
+""");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "TeacherId",
+            schema: "quizzing",
+            table: "quiz");
+    }
+}
