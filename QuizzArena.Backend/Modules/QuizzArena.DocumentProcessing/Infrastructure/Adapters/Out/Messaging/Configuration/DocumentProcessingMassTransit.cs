bb876d8bde6234be7770d75428a17d68c45961cb
Luigi-Jala <luigi.cabrera@jala.university>
2026-07-24 21:18:57 -0500

    feat(resilience): set prefetch count for consumers in DocumentProcessingMassTransit

--- QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Messaging/Configuration/DocumentProcessingMassTransit.cs
+++ QuizzArena.Backend/Modules/QuizzArena.DocumentProcessing/Infrastructure/Adapters/Out/Messaging/Configuration/DocumentProcessingMassTransit.cs
@@ -9,17 +9,17 @@ public class DocumentProcessingMassTransit
     public static void AddConsumers(IBusRegistrationConfigurator x)
     {
         x.AddSagaStateMachine<IngestionSaga, IngestionSagaState>().InMemoryRepository();
-        x.AddConsumer<TranscriptionRequestConsumer>();
+        x.AddConsumer<TranscriptionRequestConsumer>().Endpoint(e => e.PrefetchCount = 1);
 
         x.AddSagaStateMachine<GenerationSaga, GenerationSagaState>().InMemoryRepository();
-        x.AddConsumer<GenerationRequestConsumer>();
+        x.AddConsumer<GenerationRequestConsumer>().Endpoint(e => e.PrefetchCount = 1);
         x.AddConsumer<GenerationProcessingJobRequestConsumer>();
         x.AddConsumer<GenerationTerminatingProcessingRequestConsumer>();
+
+        x.AddConsumer<IndexTranscriptConsumer>().Endpoint(e => e.PrefetchCount = 1);
 
         x.AddSagaStateMachine<IndexingSaga, IndexingSagaState>()
             .InMemoryRepository();
 
-        x.AddConsumer<IndexTranscriptConsumer>()
-            .Endpoint(e => e.PrefetchCount = 1); // only process next message if the current completed
     }
 }
