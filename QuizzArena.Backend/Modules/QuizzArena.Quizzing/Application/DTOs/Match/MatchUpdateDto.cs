c912ee364abbee322a8a4184f1ade01e17c58187
RoBra18 <brand8n123@gmail.com>
2026-07-31 00:27:22 -0400

    feat: patch matches with business validations

---
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/DTOs/Match/MatchUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace QuizzArena.Quizzing.Application.DTOs.Match;
+
+public record MatchUpdateDto
+{
+    public string? Title { get; set; }
+    public DateTimeOffset? StartedAt { get; set; }
+    public DateTimeOffset? FinishedAt { get; set; }
+    public int? TimeMinutes { get; set; }
+    public int? QuestionsAmount { get; set; }
+    public int? AttemptsAmount { get; set; }
+    public bool? ShuffleQuestion { get; set; }
+    public bool? ShuffleOptions { get; set; }
+}
---
+++ QuizzArena.Backend/Modules/QuizzArena.Quizzing/Application/Validators/Match/UpdateMatchValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MatchEntity = QuizzArena.Quizzing.Domain.Entities.Match;
+
+namespace QuizzArena.Quizzing.Application.Validators.Match;
+
+public class UpdateMatchValidator : AbstractValidator<MatchEntity>
+{
+    public UpdateMatchValidator()
+    {
+        RuleFor(x => x.Title)
+           .MaximumLength(200)
+           .When(x => x.Title is not null);
+
+        RuleFor(x => x.FinishedAt)
+            .GreaterThan(x => x.StartedAt)
+            .When(x => x.FinishedAt.HasValue)
+            .WithMessage("FinishedAt must be later than StartedAt.");
+
+        RuleFor(x => x.TimeMinutes)
+            .GreaterThan(0);
+
+        RuleFor(x => x.QuestionsAmount)
+            .GreaterThan(0)
+            .When(x => x.QuestionsAmount.HasValue);
+
+        RuleFor(x => x.AttemptsAmount)
+            .GreaterThan(0);
+
+        RuleFor(x => x.CourseId)
+            .NotEmpty();
+    }
+}
