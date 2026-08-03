73add7e2f76d2c59a6b14ba5ee0a195821f67432
Luigi-Jala <luigi.cabrera@jala.university>
2026-08-03 01:16:02 -0500

    test: add unit tests for GenerationEndingConsumer, GenerationFailedConsumer, GenerationProcessingConsumer, and GenerationStartingConsumer

--- QuizzArena.Backend/QuizzArena.DocumentProcessing.Tests/Consumers/Generation/GenerationFailedConsumerTests.cs
+++ QuizzArena.Backend/QuizzArena.DocumentProcessing.Tests/Consumers/Generation/GenerationFailedConsumerTests.cs
@@ -8,7 +8,7 @@ using QuizzArena.DocumentProcessing.Domain.Enums;
 using QuizzArena.DocumentProcessing.Infrastructure.Adapters.In.Messaging.Consumers;
 using QuizzArena.DocumentProcessing.Infrastructure.Adapters.In.Messaging.Consumers.Generation;
 
-namespace QuizzArena.DocumentProcessing.Tests.Consumers;
+namespace QuizzArena.DocumentProcessing.Tests.Consumers.Generation;
 
 public class GenerationFailedConsumerTests
 {
